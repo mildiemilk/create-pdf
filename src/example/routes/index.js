9c1a0fca24d3c73404bfc4879dabc22f160bbcbb
mildiemilk <punthitra.dits@gmail.com>
2020-05-22 20:03:29 +0700

    Change path file

--- src/example/routes/index.js
+++ src/example/routes/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const generatePDF = require('../../generatePDF')
+const generatePDF = require('../generatePDF')
 
 router.get('/', async (req, res, next) => {
   const docDefinition = {
