ba6cbe3494c0aedd3b795a168103c3776a3695f8
mildiemilk <punthitra.dits@gmail.com>
2020-05-25 17:56:12 +0700

    Apply createDocDefinition with example

--- src/example/routes/index.js
+++ src/example/routes/index.js
@@ -1,14 +1,24 @@
-const express = require('express');
-const router = express.Router();
+const express = require('express')
 const generatePDF = require('../generatePDF')
+const createDocDefinition = required('../../index.js')
+
+const router = express.Router()
 
 router.get('/', async (req, res, next) => {
-  const docDefinition = {
+  const state = {
+    insured: {
+      firstname: {
+        value: 'milk'
+      }
+    }
+  }
+  const template = {
     content: [
-      'hi milk',
+      'insured.firstname.value',
       'Another paragraph, this time a little bit longer to make sure, this line will be divided into at least two lines'
     ]
   };
+  const docDefinition = createDocDefinition(template, state)
   generatePDF(docDefinition)
   res.status(200).json({
     message: 'Generate pdf success'
