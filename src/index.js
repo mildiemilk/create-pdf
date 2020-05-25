c77cf48d27ff83792d23aa3ee054f7a19cff68bf
mildiemilk <punthitra.dits@gmail.com>
2020-05-25 18:47:50 +0700

    Refactor code

--- src/index.js
+++ src/index.js
@@ -1,32 +1,27 @@
 import * as R from 'ramda'
 
+const mapArrayTemplate = (item, data, transformImageFn) => R.reduce(async (acc, cur) => [
+  ...await acc,
+  await mapTemplate(cur, data, transformImageFn),
+], [])(item)
+
 const mapTemplate = async (item, data, transformImageFn) => {
   switch (R.type(item)) {
     case 'String': return R.pathOr(item, item.split('.'), data)
-    case 'Object': return R.reduce(async (acc, key) => {
-      if (key === 'image') return {
-        ...await acc,
-        [key]: await transformImageFn(item[key]),
-      }
-      return {
-        ...await acc,
-        [key]: await mapTemplate(item[key], data, transformImageFn),
-      }
-    }, {})(R.keys(item))
-    case 'Array': return R.reduce(async (acc, cur) => [
+    case 'Object': return R.reduce(async (acc, key) => ({
       ...await acc,
-      await mapTemplate(cur, data, transformImageFn),
-    ], [])(item)
+      [key]: key === 'image' ?
+        await transformImageFn(item[key])
+        : await mapTemplate(item[key], data, transformImageFn),
+    }), {})(R.keys(item))
+    case 'Array': return mapArrayTemplate(item, data, transformImageFn)
     default:
       return item
   }
 }
 
 const createDocDefinition = async (template, data, transformImageFn = R.identity) => {
-  return R.reduce(async (acc, cur) => [
-    ...await acc,
-    await mapTemplate(cur, data, transformImageFn),
-  ], [])(template)
+  return mapArrayTemplate(template, data, transformImageFn)
 }
 
 export default createDocDefinition
