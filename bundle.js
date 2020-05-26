6eddda808f8de0bb1da26a97823b4da972c2a8b2
mildiemilk <punthitra.dits@gmail.com>
2020-05-26 12:40:46 +0700

    Add rollup to build file to umd

---
+++ bundle.js
@@ -0,0 +1,36 @@
+(function (factory) {
+  typeof define === 'function' && define.amd ? define(factory) :
+  factory();
+}((function () { 'use strict';
+
+  const R = require('ramda');
+
+  const mapArrayTemplate = (item, data, transformImageFn) => R.reduce(async (acc, cur) => [
+    ...await acc,
+    await mapTemplate(cur, data, transformImageFn),
+  ], [])(item);
+
+  const mapObjectTemplate = (item, data, transformImageFn) => R.reduce(async (acc, key) => ({
+    ...await acc,
+    [key]: key === 'image'
+      ? await transformImageFn(item[key])
+      : await mapTemplate(item[key], data, transformImageFn),
+  }), {})(R.keys(item));
+
+  const mapTemplate = async (item, data, transformImageFn) => {
+    switch (R.type(item)) {
+      case 'String': return R.pathOr(item, item.split('.'), data)
+      case 'Object': return mapObjectTemplate(item, data, transformImageFn)
+      case 'Array': return mapArrayTemplate(item, data, transformImageFn)
+      default:
+        return item
+    }
+  };
+
+  const createDocDefinition = async (template, data, transformImageFn = R.identity) => {
+    return mapObjectTemplate(template, data, transformImageFn)
+  };
+
+  module.exports = createDocDefinition;
+
+})));
