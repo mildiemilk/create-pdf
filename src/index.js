4a2d7c517a2550129acbd2b9e5f702d64aae4df0
mildiemilk <punthitra.dits@gmail.com>
2020-05-26 11:10:33 +0700

    Change template array to object

--- src/index.js
+++ src/index.js
@@ -5,15 +5,17 @@ const mapArrayTemplate = (item, data, transformImageFn) => R.reduce(async (acc, 
   await mapTemplate(cur, data, transformImageFn),
 ], [])(item)
 
+const mapObjectTemplate = (item, data, transformImageFn) => R.reduce(async (acc, key) => ({
+  ...await acc,
+  [key]: key === 'image'
+    ? await transformImageFn(item[key])
+    : await mapTemplate(item[key], data, transformImageFn),
+}), {})(R.keys(item))
+
 const mapTemplate = async (item, data, transformImageFn) => {
   switch (R.type(item)) {
     case 'String': return R.pathOr(item, item.split('.'), data)
-    case 'Object': return R.reduce(async (acc, key) => ({
-      ...await acc,
-      [key]: key === 'image'
-        ? await transformImageFn(item[key])
-        : await mapTemplate(item[key], data, transformImageFn),
-    }), {})(R.keys(item))
+    case 'Object': return mapObjectTemplate(item, data, transformImageFn)
     case 'Array': return mapArrayTemplate(item, data, transformImageFn)
     default:
       return item
@@ -21,7 +23,7 @@ const mapTemplate = async (item, data, transformImageFn) => {
 }
 
 const createDocDefinition = async (template, data, transformImageFn = R.identity) => {
-  return mapArrayTemplate(template, data, transformImageFn)
+  return mapObjectTemplate(template, data, transformImageFn)
 }
 
 export default createDocDefinition
