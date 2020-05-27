b14c61cd01409dd76b0fd76378eccee0e1137ed2
mildiemilk <punthitra.dits@gmail.com>
2020-05-27 18:54:58 +0700

    Call createDocDefinition with transformImageFn

--- src/example/routes/index.js
+++ src/example/routes/index.js
@@ -1,8 +1,25 @@
 const express = require('express')
+const R = require('ramda')
+const imageToBase64 = require('image-to-base64')
 const generatePDF = require('../generatePDF')
 const createDocDefinition = require('../../index')
 
 const router = express.Router()
+
+const transformImageFn = (key, data) => {
+  const path = R.pathOr(key, key.split('.'), data)
+  return imageToBase64(path)
+    .then(
+      (response) => {
+        return `data:image/jpeg;base64,${response}`
+      }
+    )
+    .catch(
+      (error) => {
+        console.log('err', error)
+      }
+    )
+}
 
 router.get('/', async (req, res, next) => {
   const state = {
@@ -10,15 +27,18 @@ router.get('/', async (req, res, next) => {
       firstname: {
         value: 'milk'
       }
-    }
+    },
+    attachment: { value: 'src/example/image/test.jpg' }
   }
   const template = {
     content: [
       { text: 'insured.firstname.value' },
-      'Another paragraph, this time a little bit longer to make sure, this line will be divided into at least two lines'
+      'Another paragraph, this time a little bit longer to make sure, this line will be divided into at least two lines',
+      { image: 'attachment.value' },
     ]
   };
-  const docDefinition = await createDocDefinition(template, state)
+
+  const docDefinition = await createDocDefinition(template, state, transformImageFn)
   generatePDF(docDefinition)
   res.status(200).json({
     message: 'Generate pdf success'
